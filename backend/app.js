f190e7120b187e34a77aa10f17de2e30aaaaf5b6
royricha42002 <102343693+royricha42002@users.noreply.github.com>
2024-09-01 07:48:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,22 @@ import orderRouter from './routes/orderRoute.js';
 // import { dbConnection } from "./database/dbConnection.js";
 const app = express();
 dotenv.config({ path: "./config/.env" });
-app.use(
-  cors({
-    origin: `https://fooders-restaurant.onrender.com`,
-    methods: ["POST"],
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  'https://fooders-restaurant.onrender.com',
+  'https://fooders-restaurant-frontendcode.onrender.com',
+  // Add other allowed origins here if needed
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Include credentials if required
+}));
 
 app.use(cors());
 app.use(express.json());
